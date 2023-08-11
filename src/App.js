8017e43838a0ed86e0fea403318ad8064a81d0e7
Rabyn-Collab <rabyn900@gmail.com>
2023-08-11 07:58:12 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
+import Sample from "./pages/Sample"
+import NotFound from "./pages/NotFound"
 
-function App() {
+const App = () => {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="sample_page" element={<Sample />} />
+      <Route path="*" element={<NotFound />} />
 
-export default App;
+
+    </Routes>
+  )
+}
+export default App
