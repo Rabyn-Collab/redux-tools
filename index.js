3231e5847d256576f63c609079c5f14afb08763b
Rabyn-Collab <rabyn900@gmail.com>
2023-08-31 13:43:39 +0545

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+const os = require('os');
+const path = require('path');
+console.log(os.type());
+console.log(os.version());
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.extname(__filename));
